1b79a0907ea25c19873a4006378f734bcd3d0515
MohamedELHAFA <mohamedhafa2002@gmail.com>
2025-04-09 12:10:43 +0200

    ah c'est des maths tous pass

--- index.test.js
+++ index.test.js
@@ -1,6 +1,4 @@
-// index.test.js
-
-const { add, subtract, multiply, divide } = require('./index');
+const { add, subtract, multiply, divide, absolute, square } = require('./index');
 
 test('Addition : 2 + 3 = 5', () => {
     expect(add(2, 3)).toBe(5);
@@ -20,4 +18,12 @@ test('Division : 10 / 2 = 5', () => {
 
 test('Division par zéro : doit lever une erreur', () => {
     expect(() => divide(10, 0)).toThrow("Division par zéro !");
+});
+
+test('Valeur absolue : |-5| = 5', () => {
+    expect(absolute(-5)).toBe(5);
+});
+
+test('Carré : 4² = 16', () => {
+    expect(square(4)).toBe(16);
 });
